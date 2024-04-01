faff7aefdbcbb5fd32ae7fc7460a7c30561cdbfc
LingkongSky <LingkongSky@gmail.com>
2024-04-01 16:55:19 +0800

    update the CMakefile to adaptive linux

---
+++ CMakeFiles/NonameServer.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/lingkong/work/NoNameServer/MultiPlayerManager.cpp" "CMakeFiles/NonameServer.dir/MultiPlayerManager.cpp.o" "gcc" "CMakeFiles/NonameServer.dir/MultiPlayerManager.cpp.o.d"
+  "/lingkong/work/NoNameServer/NonameServer.cpp" "CMakeFiles/NonameServer.dir/NonameServer.cpp.o" "gcc" "CMakeFiles/NonameServer.dir/NonameServer.cpp.o.d"
+  "/lingkong/work/NoNameServer/NonameServerHttp.cpp" "CMakeFiles/NonameServer.dir/NonameServerHttp.cpp.o" "gcc" "CMakeFiles/NonameServer.dir/NonameServerHttp.cpp.o.d"
+  "/lingkong/work/NoNameServer/ServerUtils.cpp" "CMakeFiles/NonameServer.dir/ServerUtils.cpp.o" "gcc" "CMakeFiles/NonameServer.dir/ServerUtils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/NonameServer.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NonameServer.dir/MultiPlayerManager.cpp.o"
+  "CMakeFiles/NonameServer.dir/MultiPlayerManager.cpp.o.d"
+  "CMakeFiles/NonameServer.dir/NonameServer.cpp.o"
+  "CMakeFiles/NonameServer.dir/NonameServer.cpp.o.d"
+  "CMakeFiles/NonameServer.dir/NonameServerHttp.cpp.o"
+  "CMakeFiles/NonameServer.dir/NonameServerHttp.cpp.o.d"
+  "CMakeFiles/NonameServer.dir/ServerUtils.cpp.o"
+  "CMakeFiles/NonameServer.dir/ServerUtils.cpp.o.d"
+  "build/NonameServer"
+  "build/NonameServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NonameServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
